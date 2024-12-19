a8b582cf30f5246dec01e87f3859667132dead8e
caaarolb <carolinelboaventura@gmail.com>
2024-12-19 12:31:19 -0300

    Criando primeiro projeto Java com a One

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2010;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.1;
+        String tipoPlano = "plus";
+
+        if(anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else  {
+            System.out.println("Filme infantil que vale a pena assistir");
+        }
+        if (incluidoNoPlano == true && tipoPlano.equals("plus")){
+            System.out.println("Filme Liberado");
+        }else {
+            System.out.println("Deve pagar pelo filme");
+        }
+    }
 }
